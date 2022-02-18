052f5faa9c3974adf51e371e549e4f40f1e300fe
David Zombar <davidzombar@gmail.com>
2022-02-18 11:54:39 -0700

    changed background color of vscode

--- db/schema.rb
+++ db/schema.rb
@@ -11,35 +11,33 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_03_231844) do
+	# These are extensions that must be enabled in order to support this database
+	enable_extension 'plpgsql'
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+	create_table 'metrics', force: :cascade do |t|
+		t.float 'chest_size'
+		t.float 'waist_size'
+		t.float 'hip_size'
+		t.float 'thigh_size'
+		t.float 'calf_size'
+		t.float 'bicep_size'
+		t.float 'forearm_size'
+		t.integer 'height_feet'
+		t.integer 'height_inches'
+		t.float 'weight_lbs'
+		t.integer 'user_id'
+		t.datetime 'created_at', precision: 6, null: false
+		t.datetime 'updated_at', precision: 6, null: false
+		t.float 'neck_size'
+	end
 
-  create_table "metrics", force: :cascade do |t|
-    t.float "chest_size"
-    t.float "waist_size"
-    t.float "hip_size"
-    t.float "thigh_size"
-    t.float "calf_size"
-    t.float "bicep_size"
-    t.float "forearm_size"
-    t.integer "height_feet"
-    t.integer "height_inches"
-    t.float "weight_lbs"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.float "neck_size"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.date "birthdate"
-  end
-
+	create_table 'users', force: :cascade do |t|
+		t.string 'first_name'
+		t.string 'last_name'
+		t.string 'username'
+		t.string 'password_digest'
+		t.datetime 'created_at', precision: 6, null: false
+		t.datetime 'updated_at', precision: 6, null: false
+		t.date 'birthdate'
+	end
 end
